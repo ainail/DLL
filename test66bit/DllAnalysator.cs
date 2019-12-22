84ef96c6e0be5131aaeea5cadef005d67414843d
ainail <ainail99@mail.ru>
2019-12-22 19:32:02 +0500

    version 2, functionality has been moved to another class

---
+++ test66bit/DllAnalysator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+
+namespace test66bit
+{
+    public class DllAnalysator : IDllAnalysator
+    {
+        private string _path;
+        public DllAnalysator(string path)
+        {
+            _path = path;
+        }
+        public IEnumerable<string> GetTypesAndMethods()
+        {
+            var typesAndMethodNames = new List<string>();
+            if (!Directory.Exists(_path))
+            {
+                typesAndMethodNames.Add("Directory does not exist");
+                return typesAndMethodNames;
+            }
+            var files = Directory.GetFiles(_path);
+
+            foreach (var file in files.Where(f => f.EndsWith(".dll")))
+            {
+                try
+                {
+                    AssemblyName.GetAssemblyName(file);
+                }
+                catch (BadImageFormatException)
+                {
+                    typesAndMethodNames.Add(file + " is not an assembley");
+                    continue;
+                }
+                var types = GetTypes(file);
+                foreach (var type in types)
+                {
+                    typesAndMethodNames.Add(type.Name);
+                    var methods = GetTypeMethods(type);
+                    foreach (var method in methods)
+                    {
+                        typesAndMethodNames.Add("\t" + method);
+                    }
+                }
+            }
+
+            IEnumerable<TypeInfo> GetTypes(string filepath)
+            {
+                var dll = Assembly.LoadFile(filepath);
+                return dll.DefinedTypes;
+            }
+
+            return typesAndMethodNames;
+        }
+        private IEnumerable<TypeInfo> GetTypes(string filepath)
+        {
+            var dll = Assembly.LoadFile(filepath);
+            return dll.DefinedTypes;
+        }
+
+        private IEnumerable<string> GetTypeMethods(TypeInfo type)
+        {
+            return type.DeclaredMethods.Where(m => m.Attributes.HasFlag(MethodAttributes.Public) ||
+             m.Attributes.HasFlag(MethodAttributes.Family)).Select(m => m.Name);
+
+        }
+    }
+}
--- test66bit/Program.cs
+++ test66bit/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.IO;
 
 namespace test66bit
 {
@@ -10,52 +7,10 @@ namespace test66bit
     {
         static void Main(string[] args)
         {
-            PrintTypeAndMethodNames(args[0]);
+            var analysator = new DllAnalysator(args[0]);
+            foreach (var str in analysator.GetTypesAndMethods())
+                Console.WriteLine(str);
         }
-        static void PrintTypeAndMethodNames(string path)
-        {
-            if (!Directory.Exists(path))
-            {
-                Console.WriteLine("Directory does not exist");
-                return;
-            }
-            var files = Directory.GetFiles(path);
-            foreach (var file in files.Where(_ => _.EndsWith(".dll")))
-            {
-                try
-                {
-                    AssemblyName.GetAssemblyName(file);
-                }
-                catch (BadImageFormatException)
-                {
-                    Console.WriteLine(file + " is not an assembley");
-                    continue;
-                }
-                var types = GetTypes(file);
-                foreach (var type in types)
-                {
-                    Console.WriteLine(type.Name);
-                    var methods = GetTypeMethods(type);
-                    foreach (var method in methods)
-                    {
-                        Console.WriteLine("\t" + method);
-                    }
-                }
-            }
-
-            IEnumerable<TypeInfo> GetTypes(string filepath)
-            {
-                var dll = Assembly.LoadFile(filepath);
-                return dll.DefinedTypes;
-            }
-
-            IEnumerable<string> GetTypeMethods(TypeInfo type)
-            {
-                return type.DeclaredMethods.Where(_ => _.Attributes.HasFlag(MethodAttributes.Public) ||
-                 _.Attributes.HasFlag(MethodAttributes.Family)).Select(_ => _.Name);
-
-            }
-        }
-
     }
+
 }
